ca31aba09741c8687e9895e5e708077a801eaf3c
BooDooOoo <42356579+BooDooOoo@users.noreply.github.com>
2021-03-24 23:58:38 +0300

    Install Django

---
+++ Reminders/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Reminders project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Reminders.settings')
+
+application = get_asgi_application()
