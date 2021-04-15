c3c7d7d18d878eb8a26b82da52fb6bdbbebfe3d0
BooDooOoo <42356579+BooDooOoo@users.noreply.github.com>
2021-04-15 18:13:39 +0300

    Register model Reminders in admin

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django import forms
+from .models import Reminders
 
-# Register your models here.
+class BoardAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Reminders
+        fields = '__all__'
+
+class BoardAdmin(admin.ModelAdmin):
+    form = BoardAdminForm
+    list_display = ('id', 'user', 'title', 'date', 'time', 'is_notificated')
+
+# Регистрация в админке модели Reminders и вывод полей
+admin.site.register(Reminders, BoardAdmin)
